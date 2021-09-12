ed34d6099ba1038c9663add8f90dbf8a964bf6cd
malcom <malcompozo@gmail.com>
2021-09-12 14:03:39 -0300

    cambios esteticos y agregado de funciones

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
+  ngOnInit() {
+  }
+
+  /* este metodo llamara al metodo mostrar por medio de una alert */
+  async salir() {
+    const alert = await this.alertController.create({
+      message: '¿Seguro deseas salir?',
+      buttons: ['Cancelar','Aceptar']
+    });
+
+    await alert.present();
+  }
 }
