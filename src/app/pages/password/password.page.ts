79cc520eecfa50412c0e9a5c0dd4401d73fcd4ae
nicolascberna <nicolascberna@gmail.com>
2021-09-13 20:28:30 -0300

    se arreglo boton

--- src/app/pages/password/password.page.ts
+++ src/app/pages/password/password.page.ts
@@ -13,11 +13,7 @@ export class PasswordPage implements OnInit {
   ngOnInit() {
   }
 
-  navigateToPage() {
-    this.router.navigate(['/restore-password']);
-  }
-
-  back(page) {
+  navigate(page) {
     this.router.navigate(page);
   }
 
