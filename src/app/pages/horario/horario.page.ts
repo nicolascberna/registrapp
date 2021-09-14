164d16c8c86763880d7ebbb37d8ed9df13217c4c
nicolascberna <nicolascberna@gmail.com>
2021-09-13 21:01:34 -0300

    cierre modal

--- src/app/pages/horario/horario.page.ts
+++ src/app/pages/horario/horario.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-horario',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HorarioPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  dismiss() {
+    this.modalController.dismiss({
+      'dismissed': true
+    });
   }
 
 }
