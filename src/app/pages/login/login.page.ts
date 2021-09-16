a64cef34ef39bb355407b9e0ec57dc182442a58e
malcom <malcompozo@gmail.com>
2021-09-16 00:31:35 -0300

    agregando validaciones en bruto y mensajes de alerta!

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -15,18 +16,38 @@ export class LoginPage implements OnInit {
     pass:''
   };
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
-  ingresar(page) {
+/*   ingresar(page) {
     const navigationExtras: NavigationExtras={
       state:{
         user: this.user.usuario
       }
     };
     this.router.navigate(page, navigationExtras);
-  }
+  } */
 
+  /* validado en bruto usuarios 'malcom', 'nicolas', contraseña '123', donde se muestra un */
+  async ingresar(page){
+    if(this.user.usuario == 'malcom' && this.user.pass == '123' || this.user.usuario == 'nicolas' && this.user.pass == '123')
+    {  const navigationExtras: NavigationExtras={
+        state:{
+          user: this.user.usuario
+        }
+      };
+      this.router.navigate(page, navigationExtras);
+    } else {
+      this.router.navigate(['/login']);
+      const alert = await this.alertController.create({
+        message: 'Usuario no registrado',
+        buttons: [{
+          text: 'Aceptar'
+      }]
+    });
+      await alert.present();
+    };
+  }
 }
